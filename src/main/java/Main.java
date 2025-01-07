c557618023865fa299755aefe7c587709a16a49d
Elias Martín González <70772713+eliazzz12@users.noreply.github.com>
2025-01-07 20:07:34 +0100

    Añadir opción para probar en local con el argumento -l. Añadir información durante el test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,28 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         String host = "sectamermelada.ddns.net";
+        if (args.length == 1 && args[0].equals("-l")) {
+            host = "localhost";
+        }
         int port = 10100;
+        System.out.println("Iniciando programa de test");
 
         try (Scanner sc = new Scanner(System.in);
              Socket socket = new Socket(host, port)) {
+            System.out.println("Conexion establecida con el servidor mermelada");
             DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
             DataInputStream dis = new DataInputStream(socket.getInputStream());
 
             String mensaje_server = dis.readUTF();
             System.out.println("El servidor mermelada dice: " + mensaje_server);
 
-            System.out.println("Escribe un mensaje para enviar al servidor:\n");
+            System.out.println("Escribe un mensaje para enviar al servidor:");
             String mensaje_client = sc.nextLine();
             System.out.println("Enviando \""+mensaje_client +"\"");
 
